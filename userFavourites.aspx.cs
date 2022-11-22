6baeb64b1480f5759f5dbf55ec58d6756190fb4b
msilv <highiqcollections@gmail.com>
2022-11-22 14:02:11 +0200

    delete

--- userFavourites.aspx.cs
+++ userFavourites.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,11 +10,24 @@ namespace WebApplication4
 {
     public partial class userFavourites : System.Web.UI.Page
     {
+        string cs = "";
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-      
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string id = GridView1.DataKeys[e.RowIndex].Value.ToString();
+            string userid = "";
+            using(SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete * from saveORG where savedID ="+id+"AND userEmail =" +userid,con);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+        }
     }
 }
