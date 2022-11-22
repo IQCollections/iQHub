1c4bec8ab6289e01bf71a57ce90a8cc0d13261de
FEG20002000 <fletcherg2457@gmail.com>
2022-11-22 21:43:20 +0200

    22/11

--- Home2.aspx.cs
+++ Home2.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,24 @@ namespace iQHub
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataTable dt = new DataTable();
+            DataTable dt2 = new DataTable();
+            string sqlCon = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ABC\source\repos\iQHub\App_Data\Xbcad.mdf;Integrated Security=True";
+            string sqlCmd = "SELECT TOP (3) [orgName],[orgEmail],[orgCategory],[orgLocation] FROM [dbo].[Organisation] ORDER BY [pageVisits] DESC";
+            string sqlCmd2 = "SELECT TOP (5) [orgName],[orgDonated] FROM [dbo].[Organisation] ORDER BY [orgDonated] DESC";
+            using (SqlConnection con = new SqlConnection(sqlCon))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sqlCmd, con);
+                SqlCommand cmd2 = new SqlCommand(sqlCmd2, con);
 
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dt);
+                SqlDataAdapter adapter2 = new SqlDataAdapter(cmd2);
+                adapter2.Fill(dt2);
+                con.Close();
+            };
+            
         }
     }
 }
